0ed9ab12640c3079906932fc36de3d21675ffb59
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2023-11-18 09:37:24 +0700

    reactjs

--- hoctap/src/components/reactdev/App.jsx
+++ hoctap/src/components/reactdev/App.jsx
@@ -2,15 +2,25 @@ import React, {useState} from "react";
 
 function App(){
     const[headingText, setHeadingText] = useState("Hello");
+    const[isMouseOver,setMouseOver] =useState(false);
 
     function handleClick(){
         setHeadingText("submitted");
+    }
+    function handleMouseOver(){
+        setMouseOver(true);
+    }
+    function handleMouseOut(){
+        setMouseOver(false);
     }
 
     return(
         <div>
             <h1>{headingText}</h1>
             <button
+            style={{backgroundColor:isMouseOver ? "red":"blue"}}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
             onClick={handleClick}>Submit</button>
         </div>
     );
