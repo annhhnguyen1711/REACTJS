88742731479609d787b56436f0d1d896c9143ff7
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2023-11-24 14:42:07 +0700

    icon reactjs

---
+++ hoctap/src/Button.jsx
@@ -0,0 +1,12 @@
+Bexport default function Button(props){
+
+    return(
+        <Button
+         className={'btn ${props.disabled? "disable'} 
+            onClick={props.onClick} 
+            disabled={props.disabled}
+        >
+        {props.text}
+        </Button>
+    );
+}
