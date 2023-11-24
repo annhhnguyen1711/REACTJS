a105d9724e78624bda4d77afeb0ebe7329edeee0
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2023-11-24 14:57:27 +0700

    Update Input.jsx

--- hoctap/src/Input.jsx
+++ hoctap/src/Input.jsx
@@ -4,8 +4,12 @@ export default function Input(props){
     return(
 
         <div
-         className={'input-group ${!props.isValid && props,isTouched? "invalid": "" }'}
-         >
+         className={'input-group ${!props.isValid && props,isTouched? "invalid":  }'}
+
+        >
+        <label htmlFor={props.name}>{props.label}</label>
+        <input className="input" name={props.name} type={props.type} placeholder={props.placeholder} value={props.value} onChange={(e)=> props.onChange(e)}>
+        </input>
         </div>
     );
 }
