e990a4f64200426c59577d30bcb3840b98011fe1
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2023-11-25 10:53:49 +0700

    api

--- hoctap/src/components/FetchExample.js
+++ hoctap/src/components/FetchExample.js
@@ -1,2 +1,13 @@
 //Các thao tác: GET, PUT, POST, DELETE
+import { useState } from "react";
 import React {useState,useEffect} from "react";
+
+
+function fetchExample(){
+    const [data,setData] = useState(null)
+    return(
+        <div>
+            {data ? {JSON.stringify(data,null,2)} :'Data loading...'  }
+        </div>
+    );
+}
